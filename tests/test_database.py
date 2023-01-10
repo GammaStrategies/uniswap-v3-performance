6f26ba81c642d9e81e85ab6e42eaf7a0e1c38157
iospap <pau@nabbo.com>
2023-01-10 15:02:58 +0100

    minors

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,4 +1,3 @@
-
 import sys
 import os
 import datetime as dt
@@ -14,18 +13,15 @@ sys.path.append(PARENT_FOLDER)
 
 from dbdata import db_managers
 from v3data.constants import PROTOCOL_UNISWAP_V3
-#from v3data.common import hypervisor
+
+# from v3data.common import hypervisor
 from v3data.hypes import impermanent_data, fees_yield
 
 
-CHAIN = "mainnet" # polygon
+CHAIN = "mainnet"  # polygon
 mongo_srv_url = "mongodb://10.10.0.39:49154"
 db_name = "gamma_v1"
-collections = {
-    "static": {"id":True},
-    "returns":{"id":True}
-}
-
+collections = {"static": {"id": True}, "returns": {"id": True}}
 
 
 async def simulate_query():
@@ -35,21 +31,23 @@ async def simulate_query():
 
     block = timestamp = 0
     result = dict()
-    for days in [1,7,30]:
-        # init 
+    for days in [1, 7, 30]:
+        # init
         result[days] = dict()
 
         # add ilg to result
-        all_dta = impermanent_data.ImpermanentData(period_days=days,protocol=PROTOCOL_UNISWAP_V3,chain=CHAIN)
-        returns_dta = await all_dta.get_feesYield_data()
-        imperm_dta = await all_dta.get_impermanent_data(get_data=False)
+        all_data = impermanent_data.ImpermanentData(
+            period_days=days, protocol=PROTOCOL_UNISWAP_V3, chain=CHAIN
+        )
+        returns_data = await all_data.get_fees_yield()
+        imperm_data = await all_data.get_impermanent_data(get_data=False)
 
         # get block n timestamp
-        block = all_dta.data["current_block"]
-        timestamp = all_dta._block_ts_map[block]
+        block = all_data.data["current_block"]
+        timestamp = all_data._block_ts_map[block]
 
         # fee yield data process
-        for k,v in returns_dta.items():
+        for k, v in returns_data.items():
             if not k in result[days].keys():
                 result[days][k] = dict()
                 # no ilg data for this hypervisor
@@ -61,62 +59,59 @@ async def simulate_query():
                 result[days][k]["block"] = block
                 result[days][k]["timestamp"] = timestamp
             result[days][k]["return"] = {
-                        "feeApr": v["feeApr"],
-                        "feeApy": v["feeApy"],
-                        "hasOutlier": v["hasOutlier"]
-                        }
-        
-        # impermanent data process 
-        for k,v in imperm_dta.items():
-            # only hypervisors with FeeYield data 
+                "feeApr": v["feeApr"],
+                "feeApy": v["feeApy"],
+                "hasOutlier": v["hasOutlier"],
+            }
+
+        # impermanent data process
+        for k, v in imperm_data.items():
+            # only hypervisors with FeeYield data
             if k in result[days].keys():
                 result[days][k]["ilg"] = {
-                        "vs_HODL_USD": v["vs_HODL_USD"],
-                        "vs_HODL_deposited": v["vs_HODL_deposited"],
-                        "vs_HODL_token0": v["vs_HODL_token0"],
-                        "vs_HODL_token1": v["vs_HODL_token1"]
-                        }
-
+                    "vs_HODL_USD": v["vs_HODL_USD"],
+                    "vs_HODL_deposited": v["vs_HODL_deposited"],
+                    "vs_HODL_token0": v["vs_HODL_token0"],
+                    "vs_HODL_token1": v["vs_HODL_token1"],
+                }
 
     # end time log
     _timelapse = dt.datetime.utcnow() - _startime
-    print("         took {:,.2f} seconds to prepare items for database load".format(_timelapse.total_seconds()))
+    print(
+        "         took {:,.2f} seconds to prepare items for database load".format(
+            _timelapse.total_seconds()
+        )
+    )
 
     # start time log
     _startime = dt.datetime.utcnow()
     _items = 0
 
-
     # create database manager/connector
-    db_connector = db_managers.MongoDbManager(url=mongo_srv_url, db_name=db_name, collections=collections)
+    db_connector = db_managers.MongoDbManager(
+        url=mongo_srv_url, db_name=db_name, collections=collections
+    )
 
     # add item by item to database
-    for period,hypervisors in result.items():
+    for period, hypervisors in result.items():
 
         for hyp_id, hyp in hypervisors.items():
             # convert int to string ( mongo 8bit int)
             # value = { k:str(v) for k,v in value.items()}
             # add to mongodb
             db_connector.add_item(coll_name="returns", item_id=hyp["id"], data=hyp)
-            _items+=1
-
+            _items += 1
 
             # try add 2 times same data ( replacement test)
             db_connector.add_item(coll_name="returns", item_id=hyp["id"], data=hyp)
 
-
     # end time log
     _timelapse = dt.datetime.utcnow() - _startime
-    print("         took {:,.2f} seconds to add {} items to the database".format(_timelapse.total_seconds(),_items))
-    
-
-
-
-
-
-
-
-
+    print(
+        "         took {:,.2f} seconds to add {} items to the database".format(
+            _timelapse.total_seconds(), _items
+        )
+    )
 
 
 # TESTING
@@ -128,5 +123,8 @@ if __name__ == "__main__":
 
     # end time log
     _timelapse = dt.datetime.utcnow() - _startime
-    print(" took {:,.2f} seconds to complete the script".format(_timelapse.total_seconds()))
-    
+    print(
+        " took {:,.2f} seconds to complete the script".format(
+            _timelapse.total_seconds()
+        )
+    )
