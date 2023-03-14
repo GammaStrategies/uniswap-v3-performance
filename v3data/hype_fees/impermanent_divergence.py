feb9e4ae81337adfce2efca359c3a80abc7b18ae
iospap <pau@nabbo.com>
2023-03-14 11:17:12 +0100

    refactoring

--- v3data/hype_fees/impermanent_divergence.py
+++ v3data/hype_fees/impermanent_divergence.py
@@ -47,9 +47,13 @@ class ImpermanentDivergence:
         """Gain/loss from staying in vault, denominated in USD"""
         initial_tokens_initial_price = (
             self.data.initial.tvl_usd / self.data.initial.total_supply.adjusted
+            if self.data.initial.total_supply.adjusted
+            else 0
         )
         latest_tokens_latest_price = (
             self.data.latest.tvl_usd / self.data.latest.total_supply.adjusted
+            if self.data.latest.total_supply.adjusted
+            else 0
         )
 
         return (
@@ -57,97 +61,165 @@ class ImpermanentDivergence:
                 (latest_tokens_latest_price - initial_tokens_initial_price)
                 / initial_tokens_initial_price
             )
-            if initial_tokens_initial_price != 0
+            if initial_tokens_initial_price
+            else 0
+        )
+
+    def hold_fifty_tokens_usd(self):
+        """Gain/loss from holding 50% / 50% tokens outside of vault, denominated in USD"""
+        total_ini_value = (
+            self.data.initial.tvl.value0.adjusted * self.data.initial.price.value0
+        ) + (self.data.initial.tvl.value1.adjusted * self.data.initial.price.value1)
+        # calc fifty percent total in each token
+        total_token0_qtty = (
+            ((total_ini_value * 0.5) / self.data.initial.price.value0)
+            if self.data.initial.price.value0
+            else 0
+        )
+        total_token1_qtty = (
+            ((total_ini_value * 0.5) / self.data.initial.price.value1)
+            if self.data.initial.price.value1
+            else 0
+        )
+
+        initial_tokens_initial_price = (
+            total_ini_value / self.data.initial.total_supply.adjusted
+            if self.data.initial.total_supply.adjusted
+            else 0
+        )
+
+        initial_tokens_latest_price = (
+            (
+                (total_token0_qtty * self.data.latest.price.value0)
+                + (total_token1_qtty * self.data.latest.price.value1)
+            )
+            / self.data.initial.total_supply.adjusted
+            if self.data.initial.total_supply.adjusted
+            else 0
+        )
+
+        return (
+            (
+                (initial_tokens_latest_price - initial_tokens_initial_price)
+                / initial_tokens_initial_price
+            )
+            if initial_tokens_initial_price
             else 0
         )
 
     def hold_initial_tokens_usd(self):
-        """Gain/loss from holding initial tokens outside of vault, denominated in USD"""
+        """Gain/loss from holding initial supplied tokens outside of vault, denominated in USD"""
+
+        initial_tokens_initial_price = (
+            (
+                (self.data.initial.tvl.value0.adjusted * self.data.initial.price.value0)
+                + (
+                    self.data.initial.tvl.value1.adjusted
+                    * self.data.initial.price.value1
+                )
+            )
+            / self.data.initial.total_supply.adjusted
+            if self.data.initial.total_supply.adjusted
+            else 0
+        )
+
         initial_tokens_latest_price = (
-            (self.data.initial.tvl.value0.adjusted * self.data.latest.price.value0)
-            + (self.data.initial.tvl.value1.adjusted * self.data.latest.price.value1)
-        ) / self.data.initial.total_supply.adjusted
-
-        latest_tokens_latest_price = (
-            self.data.latest.tvl_usd / self.data.latest.total_supply.adjusted
-        )
-
-        return (
             (
-                (latest_tokens_latest_price - initial_tokens_latest_price)
-                / initial_tokens_latest_price
+                (self.data.initial.tvl.value0.adjusted * self.data.latest.price.value0)
+                + (
+                    self.data.initial.tvl.value1.adjusted
+                    * self.data.latest.price.value1
+                )
             )
-            if initial_tokens_latest_price != 0
+            / self.data.initial.total_supply.adjusted
+            if self.data.initial.total_supply.adjusted
             else 0
         )
 
-    def deposit_in_vault_token0(self):
-        """Gain/loss from depositing in vault, denominated in token 0"""
-        initial_tokens_initial_price = (
-            self.data.initial.tvl.value0.adjusted
-            + (
-                self.data.initial.tvl.value1.adjusted
-                * (
-                    self.data.initial.price.value1 / self.data.initial.price.value0
-                    if self.data.initial.price.value0 > 0
-                    else 0
-                )
-            )
-        ) / self.data.initial.total_supply.adjusted
-
-        latest_tokens_latest_price = (
-            self.data.latest.tvl.value0.adjusted
-            + (
-                self.data.latest.tvl.value1.adjusted
-                * (
-                    self.data.latest.price.value1 / self.data.latest.price.value0
-                    if self.data.latest.price.value0 > 0
-                    else 0
-                )
-            )
-        ) / self.data.latest.total_supply.adjusted
-
         return (
             (
-                (latest_tokens_latest_price - initial_tokens_initial_price)
+                (initial_tokens_latest_price - initial_tokens_initial_price)
                 / initial_tokens_initial_price
             )
-            if initial_tokens_initial_price != 0
+            if initial_tokens_initial_price
             else 0
         )
 
-    def deposit_in_vault_token1(self):
-        """Gain/loss from depositing in vault, denominated in token 1"""
-        initial_tokens_initial_price = (
+    def hold_token0(self):
+        """Gain/loss from holding token9 outside the vault, denominated in token usd"""
+
+        total_ini_value = (
+            self.data.initial.tvl.value0.adjusted * self.data.initial.price.value0
+        ) + (self.data.initial.tvl.value1.adjusted * self.data.initial.price.value1)
+
+        # calc token quantity
+        total_token0_qtty = self.data.initial.tvl.value0.adjusted + (
             self.data.initial.tvl.value1.adjusted
-            + (
-                self.data.initial.tvl.value0.adjusted
-                * (
-                    self.data.initial.price.value0 / self.data.initial.price.value1
-                    if self.data.initial.price.value1 > 0
-                    else 0
-                )
+            * (
+                self.data.initial.price.value1 / self.data.initial.price.value0
+                if self.data.initial.price.value0
+                else 0
             )
-        ) / self.data.initial.total_supply.adjusted
+        )
 
-        latest_tokens_latest_price = (
-            self.data.latest.tvl.value1.adjusted
-            + (
-                self.data.latest.tvl.value0.adjusted
-                * (
-                    self.data.latest.price.value0 / self.data.latest.price.value1
-                    if self.data.latest.price.value1 > 0
-                    else 0
-                )
-            )
-        ) / self.data.latest.total_supply.adjusted
+        initial_token0_initial_price = (
+            total_ini_value / self.data.initial.total_supply.adjusted
+            if self.data.initial.total_supply.adjusted
+            else 0
+        )
+
+        initial_token0_latest_price = (
+            (total_token0_qtty * self.data.latest.price.value0)
+            / self.data.initial.total_supply.adjusted
+            if self.data.initial.total_supply.adjusted
+            else 0
+        )
 
         return (
             (
-                (latest_tokens_latest_price - initial_tokens_initial_price)
-                / initial_tokens_initial_price
+                (initial_token0_latest_price - initial_token0_initial_price)
+                / initial_token0_initial_price
             )
-            if initial_tokens_initial_price != 0
+            if initial_token0_initial_price != 0
+            else 0
+        )
+
+    def hold_token1(self):
+        """Gain/loss from holding token1 outside the vault, denominated in token usd"""
+
+        total_ini_value = (
+            self.data.initial.tvl.value0.adjusted * self.data.initial.price.value0
+        ) + (self.data.initial.tvl.value1.adjusted * self.data.initial.price.value1)
+
+        # calc token quantity
+        total_token1_qtty = self.data.initial.tvl.value1.adjusted + (
+            self.data.initial.tvl.value0.adjusted
+            * (
+                self.data.initial.price.value0 / self.data.initial.price.value1
+                if self.data.initial.price.value1
+                else 0
+            )
+        )
+
+        initial_token1_initial_price = (
+            total_ini_value / self.data.initial.total_supply.adjusted
+            if self.data.initial.total_supply.adjusted
+            else 0
+        )
+
+        initial_token1_latest_price = (
+            (total_token1_qtty * self.data.latest.price.value1)
+            / self.data.initial.total_supply.adjusted
+            if self.data.initial.total_supply.adjusted
+            else 0
+        )
+
+        return (
+            (
+                (initial_token1_latest_price - initial_token1_initial_price)
+                / initial_token1_initial_price
+            )
+            if initial_token1_initial_price
             else 0
         )
 
@@ -157,22 +229,49 @@ class ImpermanentDivergence:
             and self.data.latest.total_supply.raw > 0
         )
 
+        # warn over weird data
+        self.check_inconsistencies()
+
         return {
             "id": self.data.latest.hypervisor,
             "symbol": self.data.latest.symbol,
-            "blocks_passed": self.data.latest.block - self.data.initial.block,
+            "ini_block": self.data.initial.block,
+            "end_block": self.data.latest.block,
+            "ini_timestamp": self.data.initial.timestamp,
+            "end_timestamp": self.data.latest.timestamp,
             "seconds_passed": self.data.latest.timestamp - self.data.initial.timestamp,
-            "vs_hodl_usd": self.deposit_in_vault_usd() if valid_total_supply else 0,
-            "vs_hodl_deposited": self.hold_initial_tokens_usd()
+            "lping": self.deposit_in_vault_usd() if valid_total_supply else 0,
+            "hodl_deposited": self.hold_initial_tokens_usd()
             if valid_total_supply
             else 0,
-            "vs_hodl_token0": self.deposit_in_vault_token0()
-            if valid_total_supply
-            else 0,
-            "vs_hodl_token1": self.deposit_in_vault_token1()
-            if valid_total_supply
-            else 0,
+            "hodl_fifty": self.hold_fifty_tokens_usd() if valid_total_supply else 0,
+            "hodl_token0": self.hold_token0() if valid_total_supply else 0,
+            "hodl_token1": self.hold_token1() if valid_total_supply else 0,
         }
+
+    def check_inconsistencies(self):
+        """Warn on data inconsistencies"""
+        for item in [self.data.initial, self.data.latest]:
+            if not item.price.value0:
+                logger.warning(
+                    " {}'s {} {} token 0 usd price is zero at block {}  [ hype address {}]".format(
+                        self.chain,
+                        self.protocol,
+                        item.symbol,
+                        item.block,
+                        item.hypervisor,
+                    )
+                )
+            if not item.price.value1:
+                logger.warning(
+                    " {}'s {} {} token 1 usd price is zero at block {}  [ hype address {}]".format(
+                        self.chain,
+                        self.protocol,
+                        item.symbol,
+                        item.block,
+                        item.hypervisor,
+                    )
+                )
 
 
 async def impermanent_divergence_all(
