a4f427b001f17531804b0e7926b125e12a32d5fc
iospap <pau@nabbo.com>
2023-03-15 15:16:56 +0100

    added  analysis  return Points for all ( bsc left)

--- v3data/routers/arbitrum.py
+++ v3data/routers/arbitrum.py
@@ -93,6 +93,44 @@ async def hypervisor_uncollected_fees(hypervisor_address: str):
     )
 
 
+@router.get("/hypervisor/{hypervisor_address}/analytics/basic/daily")
+@cache(expire=APY_CACHE_TIMEOUT)
+async def hypervisor_analytics_basic_daily(hypervisor_address: str, response: Response):
+    return await v3data.common.analytics.get_hype_data(
+        chain=CHAIN, hypervisor_address=hypervisor_address, period=1
+    )
+
+
+@router.get("/hypervisor/{hypervisor_address}/analytics/basic/weekly")
+@cache(expire=APY_CACHE_TIMEOUT)
+async def hypervisor_analytics_basic_weekly(
+    hypervisor_address: str, response: Response
+):
+    return await v3data.common.analytics.get_hype_data(
+        chain=CHAIN, hypervisor_address=hypervisor_address, period=7
+    )
+
+
+@router.get("/hypervisor/{hypervisor_address}/analytics/basic/biweekly")
+@cache(expire=APY_CACHE_TIMEOUT)
+async def hypervisor_analytics_basic_biweekly(
+    hypervisor_address: str, response: Response
+):
+    return await v3data.common.analytics.get_hype_data(
+        chain=CHAIN, hypervisor_address=hypervisor_address, period=14
+    )
+
+
+@router.get("/hypervisor/{hypervisor_address}/analytics/basic/monthly")
+@cache(expire=APY_CACHE_TIMEOUT)
+async def hypervisor_analytics_basic_monthly(
+    hypervisor_address: str, response: Response
+):
+    return await v3data.common.analytics.get_hype_data(
+        chain=CHAIN, hypervisor_address=hypervisor_address, period=30
+    )
+
+
 @router.get("/hypervisors/aggregateStats")
 async def aggregate_stats(response: Response):
     aggregate_stats = v3data.common.hypervisor.AggregateStats(PROTOCOL, CHAIN, response)
