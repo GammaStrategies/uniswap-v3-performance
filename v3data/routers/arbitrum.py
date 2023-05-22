d6e3baa5bbc4c0a40d915cc04d70e8d633697acf
loc <l0c4t0r@gmail.com>
2023-05-22 03:00:32 +0000

    add allRewards2 for uni/arb

--- v3data/routers/arbitrum.py
+++ v3data/routers/arbitrum.py
@@ -246,6 +246,20 @@ async def impermanent_divergence_monthly(response: Response):
     return await impermanent.run(first=RUN_FIRST)
 
 
+@router.get("/allRewards2")
+@cache(expire=DB_CACHE_TIMEOUT)
+async def all_rewards_2(response: Response):
+    masterchef_v2_info = v3data.common.masterchef_v2.AllRewards2(
+        PROTOCOL, CHAIN, response
+    )
+    return await masterchef_v2_info.run(RUN_FIRST)
+
+
+@router.get("/userRewards2/{user_address}")
+async def user_rewards_2(user_address):
+    return await v3data.common.masterchef_v2.user_rewards(PROTOCOL, CHAIN, user_address)
+
+
 @router.get("/user/{address}")
 async def user_data(address: str):
     return await v3data.common.users.user_data(PROTOCOL, CHAIN, address)
